4c59e479486d59cf7efd5e187394a5d698a54ddf
Florent Peterschmitt <florent@peterschmitt.fr>
2017-03-01 11:27:42 +0100

    Update flaskapi.py

--- flaskapi.py
+++ flaskapi.py
@@ -5,53 +5,55 @@ from flask_restful import Api, Resource
 
 class APIResource(Resource):
 
-    _ROOT = '/'
-    _REGISTER = {}
+    _API_ROOT = '/'
+    _API_REGISTER = {}
+    _API_FPATH = None
 
     @classmethod
     def register_root(cls, api, root=''):
-        api.add_resource(cls, root + cls._ROOT)
+        api.add_resource(cls, root + cls._API_ROOT)
 
         cls._register(api, root)
 
     @classmethod
     def _register(cls, api, root):
-        root = root + cls._ROOT
-        for path, res in cls._REGISTER.items():
+        root = root + cls._API_ROOT
+        for path, res in cls._API_REGISTER.items():
             res_path = '{}{}'.format(root, path)
+            res._API_FPATH = res_path
             api.add_resource(res, res_path)
             res._register(api, root)
 
 class APILvl1Lvl2(APIResource):
 
     def get(self):
-        return {'path': '/lvl1/lvl2'}
+        return {'path': self._API_FPATH}
 
 class APILvl1(APIResource):
 
     class APILvl1SID(APIResource):
 
         def get(self, sid):
-            return {'sid': sid}
+            return {'sid': sid, 'path': self._API_FPATH}
 
-    _ROOT = 'lvl1/'
-    _REGISTER = {
+    _API_ROOT = 'lvl1/'
+    _API_REGISTER = {
             '<int:sid>': APILvl1SID,
             'lvl2/': APILvl1Lvl2
         }
 
     def get(self):
-        return {'path': '/lvl1'}
+        return {'path': self._API_FPATH}
 
 class APIRoot(APIResource):
 
-    _ROOT = '/'
-    _REGISTER = {
+    _API_ROOT = '/'
+    _API_REGISTER = {
             'lvl1/': APILvl1,
         }
 
     def get(self):
-        return {'path': self._ROOT}
+        return {'path': self._API_FPATH}
 
 def main():
     app = Flask(__name__)
