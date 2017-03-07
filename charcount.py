c70ca2ac4e58fede4fbf9bd21f10cd071536ba8a
Florent Peterschmitt <florent@peterschmitt.fr>
2017-03-07 22:03:40 +0000

    //

--- charcount.py
+++ charcount.py
@@ -4,6 +4,9 @@ import argparse
 import encodings
 import json
 import operator
+import os
+
+from multiprocessing import Pool
 
 def get_encs():
     return encodings.aliases.aliases.values()
@@ -14,7 +17,8 @@ def count_chars(chars):
     prev_char = None
 
     for char in chars:
-        if char in ["\n", "\t", "\r", " "]:
+        char = char.lower()
+        if char in ["\n", "\t", "\r", " ", " "]:
             prev_char = None
             next
 
@@ -28,14 +32,17 @@ def count_chars(chars):
             else:
                 char_count[char] = 1
 
-        if prev_char is not None and char not in ["\n", "\t", "\r", " "]:
+        if prev_char is not None and char not in ["\n", "\t", "\r", " ", " "]:
             char_tuple = f'{prev_char}{char}'
 
             if char_tuple in tuple_count:
                 tuple_count[char_tuple] += 1
             else:
                 tuple_count[char_tuple] = 1
-        elif char not in ["\n", "\t", "\r", " "]:
+
+            prev_char = char
+
+        elif prev_char is None and char not in ["\n", "\t", "\r", " ", " "]:
             prev_char = char
 
         else:
@@ -67,39 +74,28 @@ def sort_res(ffile):
     for sl in list_sorted:
         print('{}: {}'.format(sl[0], sl[1]))
 
-def count_file_chars():
-    flist = []
+def count_file_chars(ffile):
     char_count = {}
     tuple_count = {}
-
-    with open('flist', 'r') as fhflist:
-        flist = map(str.strip, fhflist.readlines())
+    instance = os.getpid()
 
     encs = get_encs()
     encs = ['utf-8',]
     encs.extend(encs)
 
-    for ffile in flist:
-        print(f'processing file {ffile}')
-        for enc in encs:
-            with open(ffile, 'r', encoding=enc) as fhffile:
-                try:
-                    file_content = fhffile.read()
-                    count_result = count_chars(file_content)
-                    char_count = merge_char_count(char_count, count_result[0])
-                    tuple_count = merge_char_count(tuple_count, count_result[1])
+    print(f'processing file {ffile}')
+    for enc in encs:
+        with open(ffile, 'r', encoding=enc) as fhffile:
+            try:
+                file_content = fhffile.read()
+                count_result = count_chars(file_content)
+                char_count = merge_char_count(char_count, count_result[0])
+                tuple_count = merge_char_count(tuple_count, count_result[1])
 
-                except Exception:
-                    break
+            except Exception:
+                break
 
-    res0 = json.dumps(char_count)
-    res1 = json.dumps(tuple_count)
-
-    with open('char.res.json', 'w') as fres:
-        fres.write(res0)
-
-    with open('tuple.res.json', 'w') as fres:
-        fres.write(res1)
+    return char_count, tuple_count
 
 def main():
     parser = argparse.ArgumentParser()
@@ -109,7 +105,28 @@ def main():
     args = parser.parse_args()
 
     if args.count:
-        count_file_chars()
+        with open('flist', 'r') as fhflist:
+            flist = list(map(str.strip, fhflist.readlines()))
+
+        with Pool() as p:
+            res = p.map(count_file_chars, flist)
+
+        cc = {}
+        tc = {}
+
+        for r in res:
+            cr = r[0]
+            tr = r[1]
+
+            cc = merge_char_count(cc, cr)
+            tc = merge_char_count(tc, tr)
+
+        with open('char.res.json', 'w') as fc:
+            fc.write(json.dumps(cc))
+
+        with open('tuple.res.json', 'w') as ft:
+            ft.write(json.dumps(tc))
+
     if args.sort:
         sort_res('char.res.json')
         sort_res('tuple.res.json')
