4bb1ed9b4c8ff2088f501971f5f1cdca26509671
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2017-03-30 18:05:10 +0200

    workers everywhere

--- charcount.py
+++ charcount.py
@@ -3,7 +3,7 @@
 # wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.10.7.tar.xz
 # tar xf linux-4.10.7.tar.xz
 # find linux-4.10.7 -type f > flist
-# python charcount.py -c -s -w ${CPUCOUNT_OR_SOMETHING_LIKE_THAT}
+# python charcount.py -c -m -s
 
 import argparse
 import encodings
@@ -11,6 +11,7 @@ import json
 import operator
 import os
 import sys
+import time
 
 from multiprocessing import Pool
 
@@ -36,11 +37,10 @@ def count_chars(chars):
             prev_char = None
             next
 
+        if char in char_count:
+            char_count[char] += 1
         else:
-            if char in char_count:
-                char_count[char] += 1
-            else:
-                char_count[char] = 1
+            char_count[char] = 1
 
         if prev_char is not None and char not in BLANK_CHARS:
             char_tuple = f'{prev_char}{char}'
@@ -86,36 +86,38 @@ def sort_res(ffile):
     for sl in list_sorted:
         print('{}: {}'.format(sl[0], sl[1]))
 
-def count_file_chars(ffile):
-    instance = os.getpid()
+def count_file_chars(files):
+    results = []
 
-    char_count = {}
-    tuple_count = {}
+    t_count_start = time.time()
+    for ffile in files:
+        char_count = {}
+        tuple_count = {}
+        for enc in ENCS:
+            with open(ffile, 'r', encoding=enc) as fhffile:
+                try:
+                    file_content = fhffile.read()
+                    count_result = count_chars(file_content)
+                    char_count = merge_char_count(char_count, count_result[0])
+                    tuple_count = merge_char_count(tuple_count, count_result[1])
 
-    print(f'processing file {ffile}')
-    for enc in ENCS:
-        with open(ffile, 'r', encoding=enc) as fhffile:
-            try:
-                file_content = fhffile.read()
-                count_result = count_chars(file_content)
-                char_count = merge_char_count(char_count, count_result[0])
-                tuple_count = merge_char_count(tuple_count, count_result[1])
+                    results.append([char_count, tuple_count, None])
 
-                return char_count, tuple_count, None
+                    break
 
-            except ValueError as ex:
-                pass
+                except ValueError as ex:
+                    pass
 
-    return {}, {}, ffile
+    print(f'count worker {os.getpid()} processed {len(files)} files in {time.time() - t_count_start} seconds')
+    return results
 
 def process_results(results):
+    print(f'merge worker {os.getpid()} processing {len(results)} results...')
     cc = {}
     tc = {}
     failed_files = []
 
-    res_len = len(results)
-
-    for i, r in enumerate(results):
+    for r in results:
         cr = r[0]
         tr = r[1]
         if r[2] is not None:
@@ -126,55 +128,100 @@ def process_results(results):
 
     return cc, tc, failed_files
 
-def list_chunks(chunks, llist):
-    for i in range(0, len(llist), chunks):
-        yield llist[i:i + chunks]
+def variable_amount(l, n):
+    """
+    Return a list of n lists of unkwown size.
+    """
+    lf = []
+    s = int(len(l) / n)
+
+    for i in range(0, n * s, s):
+        lf.append(l[i:i+s])
+
+    if n * s < len(l):
+        for i in range(n * s, len(l)):
+            j = i % n
+            lf[j].append(l[i])
+
+    return lf
+
+def fixed_amount(l, n):
+    """
+    Return a list of:
+        unknown amount of:
+            lists of n elements.
+    """
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--count', action='store_true')
+    parser.add_argument('-m', '--merge', action='store_true')
     parser.add_argument('-s', '--sort', action='store_true')
-    parser.add_argument('-w', '--workers', required=True, help='number of workers', type=int)
+    parser.add_argument('-w', '--workers', default=os.cpu_count(), help='number of workers', type=int)
+    parser.add_argument('-b', '--batch-len', default=100, type=int)
+    parser.add_argument('-f', '--flist', default='flist')
 
     args = parser.parse_args()
 
     if args.count:
-        with open('flist', 'r') as fhflist:
+        with open(args.flist, 'r') as fhflist:
             flist = list(map(str.strip, fhflist.readlines()))
 
+        t_count_start = time.time()
         with Pool(args.workers) as p:
-            res = p.map(count_file_chars, flist)
+            res = p.map(count_file_chars, list(fixed_amount(flist, args.batch_len)))
+        t_count = time.time() - t_count_start
+
+        fres = []
+        for r in res:
+            fres.extend(r)
+        res = fres
+
+        print(f'count time: {t_count}')
+        with open(f'{args.flist}.merge.json', 'w') as f:
+            f.write(json.dumps(res))
+
+    if args.merge:
+        with open(f'{args.flist}.merge.json', 'r') as f:
+            print('loading counters...')
+            res = json.loads(f.read())
+
+        t_merge_start = time.time()
 
         while True:
             print('processing results...')
             with Pool(args.workers) as p:
-                res = p.map(process_results, list(list_chunks(args.workers, res)))
+                #res = p.map(process_results, list(variable_amount(res, args.workers)))
+                res = p.map(process_results, list(fixed_amount(res, args.batch_len)))
 
-            print(len(res))
-            if len(res) < args.workers:
+            if len(res) <= args.workers:
                 break
 
+        print('final result processing...' + str(len(res[0])))
         res = process_results(res)
 
         cc = res[0]
         tc = res[1]
         failed_files = res[2]
+        t_merge = time.time() - t_merge_start
 
-        print('writing results')
+        print(f'merge time: {t_merge}')
 
-        with open('char.res.json', 'w') as fc:
+        with open(f'{args.flist}.char.json', 'w') as fc:
             fc.write(json.dumps(cc))
 
-        with open('tuple.res.json', 'w') as ft:
+        with open(f'{args.flist}.tuple.json', 'w') as ft:
             ft.write(json.dumps(tc))
 
-        with open('flist.failed', 'w') as ff:
+        with open(f'{args.flist}.failed', 'w') as ff:
             for failed_file in failed_files:
                 ff.write(f"{failed_file}\n")
 
     if args.sort:
-        sort_res('char.res.json')
-        sort_res('tuple.res.json')
+        sort_res(f'{args.flist}.char.json')
+        sort_res(f'{args.flist}.tuple.json')
 
 if __name__ == '__main__':
     main()
