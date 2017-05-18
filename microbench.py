f1232683e365ee751103a7bd5d4368f9d0383658
Florent Peterschmitt <fpeterschmitt@capensis.fr>
2017-05-18 10:11:08 +0200

    doc

--- microbench.py
+++ microbench.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 
+"""
+import microbench
+microbench.bench_
+"""
+
 import time
 
 def bench_len_vs_try():
+    """
+    Test if array contains at least one element.
+    """
     arr_len = 10000
     tries = 1000000
     t = [x for x in range(0, arr_len)]
 
-    # Python 3.6.1: faster than try/except fail
     t1 = time.time()
     for i in range(0, tries):
         a = len(t)
     print('len(): {}'.format(time.time() - t1))
 
-    # Python 3.6.1: faster than len()
     t1 = time.time()
     for i in range(0, tries):
         try:
@@ -22,7 +28,6 @@ def bench_len_vs_try():
             pass
     print('try -> no exception: {}'.format(time.time() - t1))
 
-    # Python 3.6.1: slowest
     t1 = time.time()
     for i in range(0, tries):
         try:
