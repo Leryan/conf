7c2beb6e7b4cb7032fc0134509c5ede040070f72
Florent Peterschmitt <florent@peterschmitt.fr>
2018-06-17 17:18:03 +0200

    restore default conf, remove scrollback patch

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Fira Mono:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -49,12 +49,12 @@ static unsigned int actionfps = 30;
  * blinking timeout (set to 0 to disable blinking) for the terminal blinking
  * attribute.
  */
-static unsigned int blinktimeout = 800;
+static unsigned int blinktimeout = 0;
 
 /*
  * thickness of underline and bar cursors
  */
-static unsigned int cursorthickness = 2;
+static unsigned int cursorthickness = 3;
 
 /*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
@@ -80,35 +80,37 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 8;
+unsigned int tabspaces = 4;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+  /* 8 normal colors */
+  "#272822",
+  "#F92672",
+  "#A6E22E",
+  "#FFE792",
+  "#AE82FF",
+  "#F92672",
+  "#66D9EF",
+  "#a09a7f",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 bright colors */
 
-	[255] = 0,
+  "#0081a3",
+  "#f92672",
+  "#F92672",
+  "#A6E22E",
+  "#FFE792",
+  "#AE82FF",
+  "#F92672",
+  "#66D9EF",
+  "#a09a7f",
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+  [255] = 0,
+
+  /* more colors can be added after 255 to use with DefaultXX */
+  "#cccccc",
+  "#333333",
 };
 
 
